5590607d6f12e568d3fe7279b38b6fc95e26cfd1
Andrew Selfridge <self0001@algonquinlive.com>
2019-02-08 13:02:01 -0500

    started to integrate pickup functionality within alpha environment

--- public/js/object-pickup.js
+++ public/js/object-pickup.js
@@ -3,22 +3,26 @@ AFRAME.registerComponent('object-pickup', {
     init: function() {
         const Context_AF = this;
         Context_AF.el.isHeld = false;
+        // hold original values
         Context_AF.el.ogPos = JSON.parse(JSON.stringify( (Context_AF.el.object3D.position) ));  // have to clone so that this value wont change constantly
-        console.log(Context_AF.el.ogPos);
+        Context_AF.el.ogScale = JSON.parse(JSON.stringify( (Context_AF.el.object3D.scale) ));
+        Context_AF.el.ogRot = JSON.parse(JSON.stringify( (Context_AF.el.object3D.rotation) ));
         Context_AF.el.ogParent = Context_AF.el.object3D.parent;
 
         Context_AF.el.addEventListener('mousedown', function(event) {
-            console.log("og");
+            
+            if(!Context_AF.el.isHeld) {
+                Context_AF.el.object3D.scale.set(Context_AF.el.ogScale.x, Context_AF.el.ogScale.y, Context_AF.el.ogScale.z);
+                Context_AF.el.object3D.rotation.set(Context_AF.el.ogRot.x, Context_AF.el.ogRot.y, Context_AF.el.ogRot.z);
+            }
             Context_AF.el.isHeld = true;
             // parent to cursor
             Context_AF.el.object3D.parent = document.getElementById("cursor").object3D;
-            if(Context_AF.el.isHeld) {
-                Context_AF.el.isHeld = true;
-                Context_AF.el.object3D.position.set(0, -2.5, -2);   // using three.js for better performance
-                // Context_AF.el.setAttribute('position', {x:0,y:-2.5,z:-2});
-                Context_AF.el.object3D.visible = true;
-                document.getElementById(Context_AF.el.id + "_placeholder").object3D.visible = true;
-            }
+            Context_AF.el.object3D.position.set(0, -1.5, -0.5);   // using three.js for better performance
+            Context_AF.el.object3D.rotation.set(0,0,0);
+            // Context_AF.el.setAttribute('position', {x:0,y:-2.5,z:-2});
+            //Context_AF.el.object3D.visible = true;
+            document.getElementById(Context_AF.el.id + "_placeholder").object3D.visible = true;
         });
     }
 });
--- public/js/object-place.js
+++ public/js/object-place.js
@@ -2,20 +2,20 @@ AFRAME.registerComponent('object-place', {
     schema: {},
     init: function() {    
         const Context_AF = this;
-        // Context_AF.ogPos = Context_AF.el.getAttribute('position');
-        // Context_AF.ogParent = Context_AF.el.object3D.parent;
 
         Context_AF.el.addEventListener('mousedown', function(event) {
+            
             let object_id = Context_AF.el.id.replace('_placeholder', '');
             let object = document.querySelector("#" + object_id);
 
             if(object.isHeld) {
-                console.log("click");
                 // hide placeholder
                 Context_AF.el.object3D.visible = false;
                 // move object to placeholder location
                 object.object3D.parent = Context_AF.el.object3D;
                 object.object3D.position.set(0,0,0);
+                object.object3D.scale.set(1,1,1);
+                object.object3D.rotation.set(0,0,0);
                 object.isHeld = false;
             }
         });
