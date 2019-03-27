d8c7924c88ecd63d8fec31d59cee8bd0d1a63fd7
Andrew Selfridge <self0001@algonquinlive.com>
2019-03-27 16:19:44 -0400

    Merged master with new placeholder system

--- public/js/object-place.js
+++ public/js/object-place.js
@@ -18,6 +18,7 @@ AFRAME.registerComponent('object-place', {
         if(oculusGo) {
             // intersect with element
             el.addEventListener('raycaster-intersected', function(e){
+                console.log('intersect');
                 if(scene.triggerDown) {
                     el.intersected = true;
                     el.intersectDistance = e.detail.intersection.distance;
@@ -33,9 +34,12 @@ AFRAME.registerComponent('object-place', {
         // mobile/desktop
         else {
             el.addEventListener('mousedown', function(e) {
+                console.log(scene.selectedObject);
                 // clicked placeholder and close enough
-                if(scene.selectedObject == Context_OBJ[0].id && e.detail.intersection.distance <= maxDistance) {
-                    Context_OBJ[0].removeAttribute('static-body');
+                //scene.selectedObject == Context_OBJ[0].id && e
+                if(e.detail.intersection.distance <= maxDistance) {
+                    
+                    //Context_OBJ[0].removeAttribute('static-body');
                     Context_AF.place();
                 }
             })
@@ -48,9 +52,11 @@ AFRAME.registerComponent('object-place', {
         let scene = document.querySelector('a-scene');
 
         // store sibling object
-        let object_class = el.id.substr(0, el.id.indexOf('_'));
-        const Context_OBJ = document.getElementsByClassName(object_class);
-        let object = Context_OBJ[0];
+        // let object_class = el.id.substr(0, el.id.indexOf('_'));
+        // const Context_OBJ = document.getElementsByClassName(object_class);
+        // let object = Context_OBJ[0];
+        let object = document.querySelector('#' + scene.selectedObject);
+
         el.intersected = false;
         
         //Remove parent constraint
@@ -64,20 +70,26 @@ AFRAME.registerComponent('object-place', {
         let scale = el.object3D.scale;
         let rot = el.object3D.rotation;
         
+        console.log(object);
         object.object3D.position.set(pos.x, pos.y, pos.z);
-        object.object3D.scale.set(scale.x, scale.y, scale.z);
+        //object.object3D.scale.set(scale.x, scale.y, scale.z);
         object.object3D.rotation.set(rot._x, rot._y, rot._z);
 
         // assign physics if necessary
-        if(data.hasCollision){
+        // if(data.hasCollision){
+        //     object.setAttribute('dynamic-body', {});
+        //  }
+        if(object.components['object-pickup'].data.hasCollision){
+            object.removeAttribute('static-body');
             object.setAttribute('dynamic-body', {});
-         }
+        }
+
         
-        // hide placeholders
-        let placeholders = document.getElementsByClassName(object.classList[0] + "_placeholder");
+        // hide placeholderss
+        let placeholders = document.getElementsByClassName("placeholder");
         for(i = 0; i < placeholders.length; i++) {
             placeholders[i].object3D.visible = false
-            placeholders[i].object3D.scale.set(0.1, 0.1, 0.1);
+            //placeholders[i].object3D.scale.set(0.1, 0.1, 0.1);
         }
         // set selectedObject back to null
         scene.selectedObject = null;
@@ -90,26 +102,31 @@ AFRAME.registerComponent('object-place', {
         let scene = document.querySelector('a-scene');
 
         // store sibling object
-        let object_class = el.id.substr(0, el.id.indexOf('_'));
-        const Context_OBJ = document.getElementsByClassName(object_class);
-        let object = Context_OBJ[0];
+        // let object_class = el.id.substr(0, el.id.indexOf('_'));
+        // const Context_OBJ = document.getElementsByClassName(object_class);
+        // let object = Context_OBJ[0];
+        let object = document.querySelector('#' + scene.selectedObject);
 
         //Remove parent constraint
         object.removeAttribute('mdmu-parent-constraint');
 
         // assigning starting attributes
-        og_obj = ntw_ + object_class;
-        console.log('OG OBJ IS:', og_obj);
-        console.log('OG POS IS:', og_obj.position);
+        // og_obj = ntw_ + object_class;
+        // console.log('OG OBJ IS:', og_obj);
+        // console.log('OG POS IS:', og_obj.position);
 
-        object.object3D.position.set(og_obj.position);
-        object.object3D.rotation.set(og_obj.rotation);
+        // object.object3D.position.set(og_obj.position);
+        // object.object3D.rotation.set(og_obj.rotation);
+        //object.object3D.parent = object.ogParent;
+        object.object3D.position.set(object.ogPos.x, object.ogPos.y, object.ogPos.z);
+        object.object3D.rotation.set(object.ogRot._x, object.ogRot._y, object.ogRot._z);
+        object.object3D.scale.set(object.ogScale.x, object.ogScale.y, object.ogScale.z);
 
         // hide placeholders
-        let placeholders = document.getElementsByClassName(object.classList[0] + "_placeholder");
+        let placeholders = document.getElementsByClassName("placeholder");
         for(i = 0; i < placeholders.length; i++) {
             placeholders[i].object3D.visible = false;
-            placeholders[i].object3D.scale.set(0.1, 0.1, 0.1);
+            //placeholders[i].object3D.scale.set(0.1, 0.1, 0.1);
 
         }
 
