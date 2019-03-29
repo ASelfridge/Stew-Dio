174d508c94bb03369dc93cf62a60f4c28e9613a8
Andrew Selfridge <andrewselfridge@cmail.carleton.ca>
2019-03-29 12:50:20 -0400

    Removed spaces within code

--- public/js/object-place.js
+++ public/js/object-place.js
@@ -104,8 +104,6 @@ AFRAME.registerComponent('object-place', {
         for(i = 0; i < placeholders.length; i++) {
             placeholders[i].object3D.visible = false;
             placeholders[i].object3D.scale.set(0.1, 0.1, 0.1);
-
-
         }
 
         // set selectedObject back to null
